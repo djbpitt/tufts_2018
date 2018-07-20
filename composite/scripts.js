581f9c148d408e65541b774f8e6e19dc0d1e7ea6
Katy Felkner <katy.felkner@ou.edu>
2018-07-20 17:03:54 -0400

    comment WIP code q

--- composite/scripts.js
+++ composite/scripts.js
@@ -1,6 +1,6 @@
 //script written by Katy to attempt to highlight sentences based on the html class attribute
 
-function highlightWords() {
+/*  function highlightWords() {
 
     // 0th step: clear existing highlighting
     lis = document.getElementsByTagName("li");
@@ -12,19 +12,19 @@ function highlightWords() {
     
     // first step: get the class of a moused over element (and highlight that element)
     // listen for mouseover on <li> 
-    class = "class of this" // might be an array
+    //class = "class of this" // might be an array
     
     // second step: find all other elements, in all other languages, that match that class
-    for (i = 0; i < class.length; i++) { 
-        var same = document.getElementsByClassName(class[i])
-        for (j = 0; j < class.length; j++) {
+    //for (i = 0; i < class.length; i++) { 
+        //var same = document.getElementsByClassName(class[i])
+       // for (j = 0; j < class.length; j++) {
             // third step: highlight them!
-            same[j].style.backgroundColor='green';
-        }
+            //same[j].style.backgroundColor='green';
+     //   }
         
-    }
+   // }
 
-}
+}*/
 
 function init() {
     alert("Hello, World!");
