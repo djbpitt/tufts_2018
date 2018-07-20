3fdce8a1d5a4dd2558f4328b70c7000ef97df172
Katy Felkner <katy.felkner@ou.edu>
2018-07-20 17:17:32 -0400

    fix error

--- composite/scripts.js
+++ composite/scripts.js
@@ -38,7 +38,7 @@ function init() {
     var lis = document.getElementsByTagName("li");
     
     for (var i = 0; i < lis.length; i++) {
-        lis[i].addEventListener('mouseover', toggle, false);
+        lis[i].addEventListener('mouseover', highlightWords, false);
     }   
     
 }
