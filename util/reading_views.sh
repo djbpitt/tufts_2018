1fb6c7739882f5e0f81b01acb994f56f11b1ac50
David J Birnbaum <djbpitt@gmail.com>
2018-07-20 14:22:44 -0400

    first reading views

---
+++ util/reading_views.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+for inputDir in sources/*/tt/*; do
+  out=$(basename $(dirname $(dirname $inputDir)))
+  java -jar /opt/saxon/saxon9he.jar -s:$inputDir -xsl:util/tt_to_html.xsl -o:"reading_views/$out"
+done
